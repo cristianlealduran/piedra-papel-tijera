4dfff3523135a55596e67468b06408fc14252ca9
Cristian Leal <128062183+cristianlealduran@users.noreply.github.com>
2023-05-04 22:41:45 -0400

    Update piedrapapeltijera.rb

--- piedrapapeltijera.rb
+++ piedrapapeltijera.rb
@@ -1,11 +1,11 @@
 # primero se crean los objetos que serán escrito en el codigo cuando se ejecute como respuesta, es por esto que hay que abrir el "ruby piedrapapeldijera.rb" y colocar una de las siguentes opciones:
 opciones = ["piedra", "papel", "tijera"]
 
-# encontré este metodo en internet, porque con rand no logré configurar de la misma forma
+# encontré este metodo en internet, porque con rand no logré configurar de la misma forma a pesar de que es una opción variable
 
 computadora = opciones.sample
 
-# aqui se crea el codigo que es el jugador, se moverá desde entre los digitos "0,1,2" ya que cero cuenta cuanta como un numero
+# aqui se crea el codigo que es el jugador, se moverá desde entre los digitos "0,1,2" ya que cero cuenta cuanta como un numero. Con este puedo configurar palabras como respuestas desde el teclado.
 jugador = ARGV[0]
 
 # Verificar si la opción del jugador es válida
